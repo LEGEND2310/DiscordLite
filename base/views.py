786ded7e7f5cb1bbaecb79b682a60e78155521a9
LEGEND2310 <deepakkumarjha178531@gmail.com>
2021-12-28 01:37:38 +0530

    implemented basic crud functionality

--- base/views.py
+++ base/views.py
@@ -1,27 +1,57 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
-rooms_list = [
-    {'id': 1, 'name': 'Lets Learn Django'},
-    {'id': 2, 'name': 'Python is fun'},
-    {'id': 3, 'name': 'Design with me'},
-]
+# rooms_list = [
+#     {'id': 1, 'name': 'Lets Learn Django'},
+#     {'id': 2, 'name': 'Python is fun'},
+#     {'id': 3, 'name': 'Design with me'},
+# ]
 
 
 def home(request):
+    rooms_list = Room.objects.all()
     context = {'rooms_list': rooms_list}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-
-    for r in rooms_list:
-        if r['id'] == int(pk):
-            room = r
-            break
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {"obj": room})
