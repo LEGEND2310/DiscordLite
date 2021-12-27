91f287b6f1d4cf0552752643e8ad7850df636057
LEGEND2310 <deepakkumarjha178531@gmail.com>
2021-12-27 10:29:36 +0530

    creating base templates

--- base/views.py
+++ base/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
 
 
 def rooms(request):
-    return HttpResponse('Room List')
+    return render(request, 'room.html')
