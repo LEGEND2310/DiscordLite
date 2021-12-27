3e157a50e0db6dcbe06ec5927e50d782e8d60e4b
LEGEND2310 <deepakkumarjha178531@gmail.com>
2021-12-28 01:42:40 +0530

    removing the secret key

--- DiscordLite/settings.py
+++ DiscordLite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*-ei808rgz@u*nomn=&bi*1&f9w=^ps8@w3zjn_ez()f5aeu)'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
